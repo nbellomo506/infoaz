e9ee4b1ccf4ab0a738186512c3e034cbbd2c013e
nicolino2002 <bellomo.nicolas@panettipitagora.edu.it>
2023-01-03 20:30:11 +0100

    ciao

--- client/components/Header.vue
+++ client/components/Header.vue
@@ -12,26 +12,39 @@
 
       <div>
 
-        <b-navbar class="grey" type="dark">
 
 
 
                <!--<<b-img fluid src="logo_innovambiente.png" width="308.95" height="66"> </b-img>
               <b-img fluid src="logo_greenext.png" width="178" height="66"> </b-img>-->
 
+          <b-container fluid class="grey p-3">
+            <b-row class="ml-5">
+              <b-col class="mt-2" xl="3">
+                <b-img fluid src="../static/img/logo_innovambiente.png" width="308.95" height="66"></b-img>
+              </b-col>
+
+              <b-col xl="3" class="mt-3">
+                <b-img class="ml-5" fluid src="../static/img/logo_greenext.png" width="178" height="66"> </b-img>
+              </b-col>
+
+              <b-col  offset-xl="3" xl="3">
+                <b-img src="../static/img/logo_infowaste.png" width="232" height="99"> </b-img>
+              </b-col>
+            </b-row>
+          </b-container>
+
+          <!--
               <b-img fluid src="../static/img/logo_innovambiente.png" width="308.95" height="66"> </b-img>
               <b-img class="ml-5" fluid src="../static/img/logo_greenext.png" width="178" height="66"> </b-img>
 
 
 
             <b-navbar-nav class="ml-auto">
-
-                   <b-img src="../static/img/logo_infowaste.png" width="232" height="99"> </b-img>
-                 <!--<b-img src="logo_infowaste.png" width="232" height="99"> </b-img>-->
+                  <b-img src="../static/img/logo_infowaste.png" width="232" height="99"> </b-img>
             </b-navbar-nav>
+            -->
 
-
-        </b-navbar>
 
         <b-navbar  type="light" variant="infowaste">
           <b-container>
